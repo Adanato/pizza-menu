8cc801c50be62f98c2dd776374b1d6671d3728ea
Adam Nguyen <Adanatonw@gmail.com>
2023-06-26 23:14:00 -0400

    updated pizza list component for better control

--- src/index.js
+++ src/index.js
@@ -1,24 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pizzaData from "./data";
+//Styling
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
+  const cssss = {};
   return (
-    <div>
-      <img src="pizzas/pinaci.jpg" alt="Pizza spinaci"></img>
-      <h2> Pizza </h2>
-    </div>
+    <header className="header">
+      <h1 style={cssss}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/*<Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price={15}
+  />*/}
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
+      <div>
+        <h3> {props.pizzaObj.name}</h3>
+        <p> {props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3 + "$"}</span>
+      </div>
+    </li>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleDateString()} We're currently open
+    </footer>
   );
 }
 
