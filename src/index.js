24f49475012fc2d4abe69766f62737e2d547ad7c
Adam Nguyen <Adanatonw@gmail.com>
2023-06-27 00:45:20 -0400

    Applied array destructuring to props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import pizzaData from "./data";
 //Styling
 import "./index.css";
 
+//Main applicaton renderer
 function App() {
   return (
     <div className="container">
@@ -24,30 +25,33 @@ function Header() {
 }
 
 function Menu() {
-  const pizzas = [];
+  const pizzas = pizzaData;
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later.</p>
       )}
     </main>
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
-        <h3> {props.pizzaObj.name}</h3>
-        <p> {props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + "$"}</span>
+        <h3> {pizzaObj.name}</h3>
+        <p> {pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + "$"}</span>
       </div>
     </li>
   );
@@ -58,26 +62,36 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
+  //Conditional rendering with multiple returns
+  /* if (!isOpen) {
+    return <p>Please come back at open hours</p>;
+  } */
+
   return (
     <footer className="footer">
+      {/*Conditional rendering of jsx*/}
       {isOpen ? (
-        <div className="order">
-          <p>We're opne until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <div className="order">
           <p>
             We're closed until {openHour}:00. Come early in the morning for some
             fresh pizza.
           </p>
-          <button className="btn">Order</button>
         </div>
       )}
     </footer>
   );
 }
 
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
 //React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
